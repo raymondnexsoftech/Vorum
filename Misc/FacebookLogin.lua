977547ce8b7f5b1418bb94482d26def8ab42aa08
raymondnexsoftech <raymondwong@nexsoftech.com>
2015-05-08 18:35:47 +0800

    updated the name of the return data
    updated the name of the return data

--- Misc/FacebookLogin.lua
+++ Misc/FacebookLogin.lua
@@ -56,7 +56,7 @@ function facebookLogin.login(listener)
 				event.isLoginSuccess = true
 				event.userId = userId
 				event.sessionToken = sessionToken
-				event.facebookToken = facebookToken
+				event.fbToken = facebookToken
 			else
 				event.linkError = true
 			end
@@ -156,7 +156,7 @@ function facebookLogin.login(listener)
 			event.isLoginSuccess = true
 			event.userId = response.objectId
 			event.sessionToken = response.sessionToken
-			event.facebookToken = facebookToken
+			event.fbToken = facebookToken
 			listener(event)
 			return
 		end
