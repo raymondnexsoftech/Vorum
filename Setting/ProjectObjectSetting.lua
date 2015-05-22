ffd9e41624ed1dfa5a695d418329326433f4fb4b
Peter Chan <foren527@gmail.com>
2015-05-22 19:09:15 +0800

    open noticeScene

--- Setting/ProjectObjectSetting.lua
+++ Setting/ProjectObjectSetting.lua
@@ -62,14 +62,14 @@ end
 
 
 local function goToNoticeTabSceneFnc(event)
-	-- storyboard.gotoScene("Scene.NoticeTabScene")
-	-- global.currentSceneNumber = 4
-	local tabbar = headTabFnc.getTabbar()
-	native.showAlert(localization.getLocalization("noticeNotWorkTitle"),localization.getLocalization("noticeNotWorkDesc"),{localization.getLocalization("ok")})
-	timer.performWithDelay( 100, function(event)
-		tabbar:setSelected(global.currentSceneNumber)  
-	end
-	)
+	storyboard.gotoScene("Scene.NoticeTabScene")
+	global.currentSceneNumber = 4
+	-- local tabbar = headTabFnc.getTabbar()
+	-- native.showAlert(localization.getLocalization("noticeNotWorkTitle"),localization.getLocalization("noticeNotWorkDesc"),{localization.getLocalization("ok")})
+	-- timer.performWithDelay( 100, function(event)
+	-- 	tabbar:setSelected(global.currentSceneNumber)  
+	-- end
+	-- )
 
 end
 local function goToSettingTabSceneFnc(event)
@@ -129,7 +129,7 @@ local SETTING = {
 											        id = "notice",
 											        size = 16,
 											        labelYOffset = -8,
-											        -- onPress = goToNoticeTabSceneFnc,
+											        onPress = goToNoticeTabSceneFnc,
 											    },
 											    {
 											    	width = 59,
