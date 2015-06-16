6e66dbbc3c4b6dea90eaaa2fc7d3729b2a929dfe
Raymond Wong <raymondwong@nexsoftech.com>
2015-06-16 18:25:28 +0800

    Enabled and modified the code to show "createdBy" in search Post

--- ProjectObject/SearchScreen.lua
+++ ProjectObject/SearchScreen.lua
@@ -425,22 +425,22 @@ local function createResult(resultData)
 	group_thisResult:insert( text_comment )
 	
 	if(searchType=="post")then
-		-- local createdBy = display.newText{
-		-- 	text = localization.getLocalization("searchScreen_by")..tostring(temp_creatorName), 
-		-- 	x = display.contentWidth-10,
-		-- 	y = line_bottom.y-6,
-		-- 	width = 420,
-		-- 	height = 0	, 
-		-- 	font = "Helvetica",
-		-- 	fontSize=22
-		-- }
-		-- createdBy = display.newText(createdBy);
-		-- createdBy:setFillColor( 104/255, 104/255, 104/255 )
-		-- createdBy.anchorX=1
-		-- createdBy.anchorY=1
-		-- createdBy:addEventListener("touch",goToNewScene)
-		-- group_thisResult:insert( createdBy )
-
+		local createdBy = {
+			text = localization.getLocalization("searchScreen_by")..tostring(temp_creatorName), 
+			x = display.contentWidth - 30,
+			y = line_bottom.y-15,
+			width = 200,
+			height = 28, 
+			font = "Helvetica",
+			fontSize = 25,
+			align = "right",
+		}
+		createdBy = display.newText(createdBy);
+		createdBy:setFillColor( 104/255, 104/255, 104/255 )
+		createdBy.anchorX=1
+		createdBy.anchorY=1
+		createdBy:addEventListener("touch",goToNewScene)
+		group_thisResult:insert( createdBy )
 	end
 
 	return group_thisResult
